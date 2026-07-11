e71d4629e9c01b95a899c2b865be809f64c9b8a0
Kirill Pashinin <Kirill.Pashinin@icl-services.com>
2026-07-11 22:17:05 +1000

    add TransactionProducer

---
+++ src/Producer/Extensions/ProducersExt.cs
@@ -0,0 +1,13 @@
+﻿using Producer.Producers;
+
+namespace Producer.Extensions;
+
+public static class ProducersExt
+{
+    public static IServiceCollection RegisterProducers(this IServiceCollection services)
+    {
+        services.AddSingleton<TransactionProducer>();
+
+        return services;
+    }
+}
---
+++ src/Producer/Producers/TransactionProducer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Shared.Contracts.Commands;
+
+namespace Producer.Producers;
+
+public class TransactionProducer(IPublishEndpoint publishEndpoint, ILogger<TransactionProducer> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(Random.Shared.Next(500, 3000), stoppingToken);
+
+            var transaction = new InitiateTransactionCommand
+            {
+                TransactionId = Guid.CreateVersion7(),
+                FromUserId = Guid.CreateVersion7(),
+                ToUserId = Guid.CreateVersion7(),
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await publishEndpoint.Publish(transaction, stoppingToken);
+
+            logger.LogInformation("Transaction {TransactionId} was published", transaction.TransactionId);
+        }
+    }
+}
